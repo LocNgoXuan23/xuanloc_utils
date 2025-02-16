7b4bebd0941233ffecb17071c94e1f7021395823
LocNgoXuan2306 <locviettri@gmail.com>
2025-02-16 13:59:55 +0700

    refactor: remove requirements loading from setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
-
 setup(
     name='xuanloc_utils',
     version='0.1',
     packages=find_packages(),
-    install_requires=required,
 )
